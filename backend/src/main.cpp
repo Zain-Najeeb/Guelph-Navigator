3883ce30913551d1ed4d5f914936a9c04ed8d90d
Jacob Parker <jacobandian3@gmail.com>
2023-03-07 18:04:56 -0500

    Added crow :)

---
+++ backend/src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "crow.h"
+
+int main() {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(18080).multithreaded().run();
+
+    printf("Hello!\n");
+}
