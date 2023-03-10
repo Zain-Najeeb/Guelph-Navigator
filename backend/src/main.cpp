6a7907d9a74c938be9aa6c34093ee8355a42db6b
Jacob Parker <jacobandian3@gmail.com>
2023-03-10 00:21:05 -0500

    Added instructions to install neo4j, and installed neo4j

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include "crow.h"
+#include <neo4j-client.h>
 
 int main() {
+    neo4j_client_init();
+
+    neo4j_connection *connection = neo4j_connect("neo4j://neo4j:123@localhost", NULL, NEO4J_INSECURE);
+
+    if (connection == NULL) {
+        neo4j_perror(stderr, errno, "Connection failed");
+        return EXIT_FAILURE;
+    }
+    
+    neo4j_result_stream_t *results = neo4j_run(connection, "RETURN 'hello world'", neo4j_null);
+    if (results == NULL) {
+        neo4j_perror(stderr, errno, "Failed to run statement");
+        return EXIT_FAILURE;
+    }
+
+    neo4j_result_t *result = neo4j_fetch_next(results);
+    if (result == NULL) {
+        neo4j_perror(stderr, errno, "Failed to fetch result");
+        return EXIT_FAILURE;
+    }
+
+
+    neo4j_value_t value = neo4j_result_field(result, 0);
+    char buf[128];
+    printf("%s\n", neo4j_tostring(value, buf, sizeof(buf)));
+
+
+    neo4j_close_results(results);
+    neo4j_close(connection);
+    neo4j_client_cleanup();
+
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([](){
