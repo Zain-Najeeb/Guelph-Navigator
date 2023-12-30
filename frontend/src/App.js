debc4e45a02c11fd6ce8d16a9768d2d8a7820c84
Zain <m.zainnajeeb@gmail.com>
2023-12-30 00:49:54 -0500

    unfinished search button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import SearchBar from './components/wheretoBar';
-import './images/guelphlogo.png'
+import SquareButton from './components/squareButton';
 function App() {
+  const [currentLocation, setCurrentLocation] = useState('Current Location, (Default is the Cannon)');
+  const [findLocation, setFindLocation] = useState('Find a location');
+  const [showError, setShowError] = useState(false);
+  const handleSearch = () => {
+    if (currentLocation.trim() === '' || findLocation.trim() === '') {
+      setShowError(true);
+    } else {
+      setShowError(false);
+    }
+  };
   return (
     <div className="App">
-      <div className="LogoContainer"> 
-      <img src={require('./images/guelphlogo.png')} alt ="Guelph-logo"/>
-      </div>
-      <div className="SearchBarsContainor">  
-        <SearchBar input = "Current Location, (Default is the Cannon)" />
-        <div className="Space" /> 
-        <SearchBar input = "Find a location"/>
-       </div>
+      <div className="InteractionsContainor"> 
+        <div className="LogoContainer"> 
+          <img src={require('./images/guelphlogo.png')} alt ="Guelph-logo"/>
+        </div>
+        <div className="SearchBarsContainor">  
+          <SearchBar input={currentLocation} onChange={(value) => setCurrentLocation(value)}  />
+          <div className="Space" /> 
+          <SearchBar input={findLocation} onChange={(value) => setFindLocation(value)} />
+        </div>
+        <div className="SquareButtonContainor">
+          <SquareButton onClick={handleSearch} />
+        </div>
+        <div className = "ErrorMessage"> 
+          {showError && <p className="ErrorText"> "dsfs" </p>}
+        </div>
+      </div> 
+
     </div>
   );
 }
--- frontend/src/components/wheretoBar.js
+++ frontend/src/components/wheretoBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
-import "./wheretoBar.css";
+import './wheretoBar.css';
 
-const SearchBar = ({input}) => {
+const SearchBar = ({ input, onChange }) => {
   const [searchInput, setSearchInput] = useState("");
   const [showResults, setShowResults] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -9,27 +9,29 @@ const SearchBar = ({input}) => {
   const locations = [
     { name: "Rozanski Hall" },
     { name: "University Centre" },
-    { name: "Mackinnon" }, 
-    { name: "Athletics Centre"}, 
-    { name: "Macnaughton"}, 
-
+    { name: "Mackinnon" },
+    { name: "Athletics Centre" },
+    { name: "Macnaughton" },
   ];
 
   const searchContainerRef = useRef();
+
   const handleChange = (e) => {
     setSearchInput(e.target.value);
     setShowResults(true);
     setIsTyping(true);
+    onChange(e.target.value); 
   };
 
   const handleSelect = (location) => {
-    if ( location.name === "" && searchInput === "") {
+    if (location.name === "" && searchInput === "") {
       setShowResults(true);
     } else {
       setSearchInput(location.name);
       setShowResults(false);
     }
   };
+
   const handleKeyDown = (e) => {
     if (showResults && (e.keyCode === 9 || e.keyCode === 40)) {
       e.preventDefault();
@@ -40,9 +42,9 @@ const SearchBar = ({input}) => {
     } else if (e.keyCode === 38 && showResults) {
       e.preventDefault();
       if (selectedIndex === 0) {
-        setSelectedIndex(filteredLocations.length - 1); 
+        setSelectedIndex(filteredLocations.length - 1);
       } else {
-      setSelectedIndex((selectedIndex - 1) );
+        setSelectedIndex((selectedIndex - 1));
       }
     }
     setIsTyping(true);
@@ -58,13 +60,12 @@ const SearchBar = ({input}) => {
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
-
     };
   }, [searchContainerRef]);
 
   const filteredLocations = locations.filter((location) => {
     return (
-      (searchInput === '' && showResults) || 
+      (searchInput === '' && showResults) ||
       (searchInput !== '' && location.name.toLowerCase().includes(searchInput.toLowerCase()))
     );
   });
@@ -73,7 +74,7 @@ const SearchBar = ({input}) => {
     <div className="search-container" ref={searchContainerRef}>
       <input
         type="text"
-        placeholder= {input}  
+        placeholder={input}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
         onBlur={() => setIsTyping(false)}
@@ -85,9 +86,9 @@ const SearchBar = ({input}) => {
           {filteredLocations.map((location, index) => {
             return (
               <li
-              key={index}
-              className={selectedIndex === index ? "selected" : ""}
-              onClick={() => handleSelect(location)}
+                key={index}
+                className={selectedIndex === index ? "selected" : ""}
+                onClick={() => handleSelect(location)}
               >
                 {location.name}
               </li>
