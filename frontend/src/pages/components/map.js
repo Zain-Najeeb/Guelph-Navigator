329e26d6aa0f8ddec0d64c6836f1c096e8ef706c
Zain <m.zainnajeeb@gmail.com>
2024-01-01 12:57:29 -0500

    welcome back room number

--- frontend/src/pages/components/map.js
+++ frontend/src/pages/components/map.js
@@ -7,7 +7,7 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 import './map.css';
 import { cords } from './../../index';
 import { SearchBar } from './wheretoBar';
-import {getFastestPathPoints} from '../connections.js'
+import { getFastestPathPoints } from '../connections.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { rooms } from './../../index';
@@ -18,6 +18,7 @@ const validBuilding = (building) => {
 
 export function UniversityMap() {
   const [findLocation, setFindLocation] = useState('Search University of Guelph');
+  const [roomNumber, setRoomNumber] = useState ('Room Number (Optional)')
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [Error, setIsError] = useState(false);
@@ -134,6 +135,7 @@ export function UniversityMap() {
       });
     }
   };
+
   const handleDirectionsButtonClick = () => {
     const lowerCaseFindLocation = findLocation.toLowerCase().trim();
     if (!validBuilding(lowerCaseFindLocation)) {
@@ -161,42 +163,42 @@ export function UniversityMap() {
         <div ref={mapContainer} className="map" />
       </div>
       <div className="NavBars">
-        <div className = "destination">
-        <SearchBar input={findLocation} onChange={(value) => setFindLocation(value)} isRoom={false} />
-        <div className = "icons">
-        <button className="arrow-icon"
-          onClick={() => handleDirectionsButtonClick()}
-          style = {{
-            left: "87%"
-          }} 
-        >
-          <FontAwesomeIcon icon={faArrowRight} />
-          <div className="tooltip"
-            style={{
-              color: Error ? "red" : "white"
-            }}
-          >{directionsValue}</div>
-        </button>
-        <button
-          className="arrow-icon"
-          onClick={() => handleSearchButtonClick()} >
-          <FontAwesomeIcon icon={faSearch} />
-          <div className="search-icon-tooltip"
-            style={{
-              color: Error ? "red" : "white"
-            }}
-          >
-            {searchValue}
+        <div className="destination">
+          <SearchBar input={findLocation} onChange={(value) => setFindLocation(value)} isRoom={false} />
+          <div className="icons">
+            <button className="arrow-icon"
+              onClick={() => handleDirectionsButtonClick()}
+              style={{
+                left: "87%"
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+              <div className="tooltip"
+                style={{
+                  color: Error ? "red" : "white"
+                }}
+              >{directionsValue}</div>
+            </button>
+            <button
+              className="arrow-icon"
+              onClick={() => handleSearchButtonClick()} >
+              <FontAwesomeIcon icon={faSearch} />
+              <div className="search-icon-tooltip"
+                style={{
+                  color: Error ? "red" : "white"
+                }}
+              >
+                {searchValue}
+              </div>
+            </button>
           </div>
-        </button>
-      </div>
         </div>
+        {validBuilding(findLocation.toLowerCase()) &&(
+          <SearchBar input={roomNumber} onChange={(value) => setRoomNumber(value)} isRoom={true} building = {findLocation.toLowerCase()}/>
+        )}
       </div>
     </div>
   );
 }
 
 export default UniversityMap;
-
-
-
