5cc75fa83a09e6b68071f8e5136abaf049dd0f1c
Jacob Parker <jacobandian3@gmial.com>
2024-01-02 17:44:22 -0500

    Added building fetching and display on a testing page

--- frontend/src/pages/connections.js
+++ frontend/src/pages/connections.js
@@ -11,16 +11,21 @@ AWS.config.update({
     credentials: new AWS.Credentials(aKey, pKey), 
 })
 export const getBuildings = async () => {
-  
+  try {
+    const response = await axios.get('http://localhost:5216/Building/GetBuildings');
+    return response.data;
+  } catch(error) {
+    throw error;
+  }
 }
 export const getSpot = async () => {
- await axios.get('http://localhost:5216/Spot/GetSpot', { params: { id: 1 } })
- .then(response => {
-  console.log(response.data);
- })
- .catch(error => {
-   console.error(error);
- });
+  await axios.get('http://localhost:5216/Spot/GetSpot', { params: { id: 1 } })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 export const getFastestPathPoints = async (start, end) => {
   try {
